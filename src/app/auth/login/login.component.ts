570774ae718c0e937a91f9f5b20b0b106b68b5ed
Yaotzin Sanabria <yaosanabria@gmail.com>
2025-09-14 17:22:19 -0600

    feat(login): implement template driven form, validation and localstorage sync

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,57 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime, Subscription } from 'rxjs';
+
+const LOCAL_STORAGE_KEY = 'loginData';
 
 @Component({
-    selector: "app-login",
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.css',
-    standalone: true
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  standalone: true,
+  imports: [FormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('loginForm', { static: true }) form!: NgForm;
+  subscription: Subscription | undefined;
+
+  ngAfterViewInit() {
+    Promise.resolve().then(() => {
+      try {
+        const data = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY) || '');
+        this.form.setValue(data);
+      } catch (error) {
+          console.error('Error loading from localStorage.');
+      }
+    })
+
+    this.subscription = this.form.valueChanges
+      ?.pipe(debounceTime(500))
+      .subscribe((values) => {
+        try {
+          window.localStorage.setItem(
+            LOCAL_STORAGE_KEY,
+            JSON.stringify({
+              ...values,
+              password: '',
+            })
+          );
+        } catch (error) {
+          console.error('Error saving to localStorage.');
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  handleSubmit(form: NgForm) {
+    if (!form.valid) return;
+
+    console.log(form);
+    console.log(form.value);
+    console.log(form.valid);
+    console.log(form.control);
+  }
+}
